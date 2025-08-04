068a5dbda484808d93611fab3b77a36b4106ebdb
Tom Keenan <46943314+tkoktk@users.noreply.github.com>
2025-08-04 02:46:29 +0100

    more test cases

--- tests/decrypt.rs
+++ tests/decrypt.rs
@@ -3,10 +3,10 @@ use bip38::{Decrypt, Error as Bip38Error};
 
 #[test]
 fn test_bip38_decryption_compressed() {
-    let encrypted_key = "6PYMgbeR64ypE4g8ZQhGo7ScudV5BLz1vMFUCs49AWpW3jVNWfH6cAdTi2";
+    let private_key = "6PYMgbeR64ypE4g8ZQhGo7ScudV5BLz1vMFUCs49AWpW3jVNWfH6cAdTi2";
     let password = "strong_pass";
     
-    let result = encrypted_key.decrypt(password);
+    let result = private_key.decrypt(password);
     assert!(result.is_ok(), "Should successfully decrypt with correct password");
     
     let (private_key_bytes, compressed) = result.unwrap();
@@ -19,10 +19,42 @@ fn test_bip38_decryption_compressed() {
 #[test]
 fn test_bip38_decryption_with_generated_example() {
     //Manually generated a new wallet with the password "baysage"
-    let public_key = "1KynBkuVVJSdVkpFq7pvKCwXTKriK6CDPE";
     let private_key = "6PnMx7BZDf5xUWoPUDZaD8rbVJ5qtn95WaGuijZpwFgDuVJTTakyTvY2t9";
     let password = "baysage";
 
     let result = private_key.decrypt(password);
     assert!(result.is_ok(), "Should successfully decrypt with correct password");
+}
+
+#[test]
+fn test_bip38_decryption_with_incorrect_password() {
+    //Manually generated a new wallet with the password "baysage"
+    //Try decrypt with correct private key but wrong password
+    let private_key = "6PnMx7BZDf5xUWoPUDZaD8rbVJ5qtn95WaGuijZpwFgDuVJTTakyTvY2t9";
+    let password = "password";
+
+    let result = private_key.decrypt(password);
+    assert!(result.is_err(), "Should not decrypt");
+}
+
+#[test]
+fn test_bip38_decryption_with_incorrect_private_key() {
+    //Manually generated a new wallet with the password "baysage"
+    //Try decrypt with wrong priv key but right password
+    let private_key = "6PnMx7BZDf5xUWoPUDZaD8rbVJ5qtn95WaGuijZpwFgDuVJTTaky";
+    let password = "baysage";
+
+    let result = private_key.decrypt(password);
+    assert!(result.is_err(), "Should not decrypt");
+}
+
+#[test]
+fn test_bip38_decryption_with_incorrect_password_space() {
+    //Manually generated a new wallet with the password "baysage"
+    //Try decrypt with correct private key but wrong password (has a space)
+    let private_key = "6PnMx7BZDf5xUWoPUDZaD8rbVJ5qtn95WaGuijZpwFgDuVJTTakyTvY2t9";
+    let password = "baysage ";
+
+    let result = private_key.decrypt(password);
+    assert!(result.is_err(), "Should not decrypt");
 }
